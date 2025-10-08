acbbe6375bc54f73a5d8c2e27554491d3dc58005
Suyash Jawale <suyashjawale245@gmail.com>
2025-10-08 16:26:28 +0530

    Test New Website

---
+++ chunk-6CT2QUDB.js
@@ -0,0 +1,1 @@
+import{Xa as a,Ya as n,kb as o,ya as s}from"./chunk-EJLA3VPG.js";var c=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=s({type:t,selectors:[["app-s-search"]],decls:2,vars:0,template:function(e,i){e&1&&(a(0,"p"),o(1,"s-search works!"),n())},encapsulation:2})};export{c as SSearch};
