8bdd21f266364820b9a941135c1bb88ccb111a3e
Suyash Jawale <suyashjawale245@gmail.com>
2025-12-05 18:22:21 +0530

    Code checking

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,27 @@
+import { JsonPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+	selector: 'app-root',
+	imports: [RouterOutlet, JsonPipe],
+	templateUrl: './app.html',
+	styleUrl: './app.scss'
 })
 export class App {
-  constructor(private http: HttpClient) {
-    this.http.get("https://dashing-llama-639318.netlify.app/.netlify/functions/updates").subscribe({
-      next: data => {
-
-      }
-    })
-  }
+	dummy1 = signal<any>("");
+	dummy2 = signal<any>("");
+	constructor(private http: HttpClient) {
+		this.http.get("https://dashing-llama-639318.netlify.app/.netlify/functions/updates").subscribe({
+			next: data => {
+				this.dummy1.set(data);
+				this.http.get("https://dashing-llama-639318.netlify.app/.netlify/functions/updates").subscribe({
+					next: data1 => {
+						this.dummy2.set(data1);
+					}
+				})
+			}
+		})
+	}
 }
